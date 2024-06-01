eb9078c5d021822acfea7d5354c601af64511b9c
Brayan Ramos <brayanrcaicedo@gmail.com>
2024-05-31 23:35:36 -0500

    refactor: use async/await for endpoints

--- backend/controllers/blog.js
+++ backend/controllers/blog.js
@@ -2,22 +2,16 @@ const express = require('express')
 const blogRouter = express.Router()
 const Blog = require('../models/blog')
 
-blogRouter.get('/', (_, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
+blogRouter.get('/', async (_, response) => {
+    const blogs = await Blog.find({})
+    response.json(blogs)
 })
 
-blogRouter.post('/', (request, response) => {
+blogRouter.post('/', async (request, response) => {
     const blog = new Blog(request.body)
 
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
+    const result = await blog.save()
+    response.status(201).json(result)
 })
 
 module.exports = blogRouter
