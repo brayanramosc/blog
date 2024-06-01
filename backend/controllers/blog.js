413250f92a7943baa79c0cab740a6108b517ee77
Brayan Ramos <brayanrcaicedo@gmail.com>
2024-06-01 18:52:40 -0500

    feat: add validators and lib to handle errors

--- backend/controllers/blog.js
+++ backend/controllers/blog.js
@@ -8,10 +8,18 @@ blogRouter.get('/', async (_, response) => {
 })
 
 blogRouter.post('/', async (request, response) => {
-    const blog = new Blog(request.body)
+    const body = request.body
+
+    if (!body.title || !body.url) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const blog = new Blog(body)
 
     const result = await blog.save()
-    response.status(201).json(result)
+    return response.status(201).json(result)
 })
 
 module.exports = blogRouter
