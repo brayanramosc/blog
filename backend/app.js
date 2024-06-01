53b025792de0de85277403db432c916d8cdca77a
Brayan Ramos <brayanrcaicedo@gmail.com>
2024-06-01 18:49:13 -0500

    feat: add useful midlewares

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 const config = require('./utils/config')
 const express = require('express')
+require('express-async-errors')
 const app = express()
 const cors = require('cors')
 const blogRouter = require('./controllers/blog')
 const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 
 mongoose.connect(config.MONGODB_URI)
@@ -13,5 +15,7 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogRouter)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
