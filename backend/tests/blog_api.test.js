d0c6c7cd654d6eced8ad78faa43a14092f6302b9
Brayan Ramos <brayanrcaicedo@gmail.com>
2024-06-01 21:00:03 -0500

    test: add test for update method

--- backend/tests/blog_api.test.js
+++ backend/tests/blog_api.test.js
@@ -11,10 +11,7 @@ const api = supertest(app)
 describe('blog API', () => {
     beforeEach(async () => {
         await Blog.deleteMany({})
-
-        const blogObjects = helper.initialBlogs.map(blog => new Blog(blog))
-        const promiseArr = blogObjects.map(blog => blog.save())
-        await Promise.all(promiseArr)
+        await Blog.insertMany(helper.initialBlogs)
     })
 
     test('blogs are returned as json', async () => {
@@ -35,77 +32,113 @@ describe('blog API', () => {
         assert.strictEqual(ids.length, helper.initialBlogs.length)
     })
 
-    test('should return an error when a blog with missing title is sent', async () => {
-        const newBlog = {
-            author: "Edsger W. Dijkstra",
-            url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-            likes: 12
-        }
+    describe('add new blog', () => {
+        test('should return an error when a blog with missing title is sent', async () => {
+            const newBlog = {
+                author: "Edsger W. Dijkstra",
+                url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+                likes: 12
+            }
 
-        const res = await api
-            .post('/api/blogs')
-            .send(newBlog)
-            .expect(400)
-            .expect('Content-Type', /application\/json/)
+            const res = await api
+                .post('/api/blogs')
+                .send(newBlog)
+                .expect(400)
+                .expect('Content-Type', /application\/json/)
 
-        assert.strictEqual(res.body.error, 'content missing')
+            assert.strictEqual(res.body.error, 'content missing')
+        })
+
+        test('should return an error when a blog with missing url is sent', async () => {
+            const newBlog = {
+                title: "Canonical string reduction",
+                author: "Edsger W. Dijkstra",
+                likes: 12
+            }
+
+            const res = await api
+                .post('/api/blogs')
+                .send(newBlog)
+                .expect(400)
+                .expect('Content-Type', /application\/json/)
+
+            assert.strictEqual(res.body.error, 'content missing')
+        })
+
+        test('should add a blog with missing likes', async () => {
+            const newBlog = {
+                title: "Canonical string reduction",
+                author: "Edsger W. Dijkstra",
+                url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html"
+            }
+
+            await api
+                .post('/api/blogs')
+                .send(newBlog)
+                .expect(201)
+                .expect('Content-Type', /application\/json/)
+
+            const blogsAtEnd = await helper.blogsInDb()
+            assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
+
+            const lastBlog = blogsAtEnd[helper.initialBlogs.length]
+            assert.strictEqual(lastBlog.likes, 0)
+        })
+
+        test('should add a valid blog', async () => {
+            const newBlog = {
+                title: "Canonical string reduction",
+                author: "Edsger W. Dijkstra",
+                url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+                likes: 12
+            }
+
+            await api
+                .post('/api/blogs')
+                .send(newBlog)
+                .expect(201)
+                .expect('Content-Type', /application\/json/)
+
+            const blogsAtEnd = await helper.blogsInDb()
+            assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
+
+            const titles = blogsAtEnd.map(res => res.title)
+            assert(titles.includes(newBlog.title))
+        })
     })
 
-    test('should return an error when a blog with missing url is sent', async () => {
-        const newBlog = {
-            title: "Canonical string reduction",
-            author: "Edsger W. Dijkstra",
-            likes: 12
-        }
+    describe('deletion of a blog', () => {
+        test('should returned 204 status if id is valid', async () => {
+            const blogsAtStart = await helper.blogsInDb()
+            const blogToDelete = blogsAtStart[0]
 
-        const res = await api
-            .post('/api/blogs')
-            .send(newBlog)
-            .expect(400)
-            .expect('Content-Type', /application\/json/)
+            await api
+                .delete(`/api/blogs/${blogToDelete.id}`)
+                .expect(204)
 
-        assert.strictEqual(res.body.error, 'content missing')
+            const blogsAtEnd = await helper.blogsInDb()
+            assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1)
+
+            const titles = blogsAtEnd.map(r => r.title)
+            assert(!titles.includes(blogToDelete.title))
+        })
     })
 
-    test('should add a blog with missing likes', async () => {
-        const newBlog = {
-            title: "Canonical string reduction",
-            author: "Edsger W. Dijkstra",
-            url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html"
-        }
+    describe('update blog', () => {
+        test('should update the blog if exists', async () => {
+            const blogsAtStart = await helper.blogsInDb()
+            const blogToUpdate = blogsAtStart[0]
+            blogToUpdate.likes = 15
 
-        await api
-            .post('/api/blogs')
-            .send(newBlog)
-            .expect(201)
-            .expect('Content-Type', /application\/json/)
+            await api
+                .put(`/api/blogs/${blogToUpdate.id}`)
+                .send(blogToUpdate)
+                .expect(200)
 
-        const blogsAtEnd = await helper.blogsInDb()
-        assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
-
-        const lastBlog = blogsAtEnd[helper.initialBlogs.length]
-        assert.strictEqual(lastBlog.likes, 0)
-    })
-
-    test('should add a valid blog', async () => {
-        const newBlog = {
-            title: "Canonical string reduction",
-            author: "Edsger W. Dijkstra",
-            url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-            likes: 12
-        }
-
-        await api
-            .post('/api/blogs')
-            .send(newBlog)
-            .expect(201)
-            .expect('Content-Type', /application\/json/)
-
-        const blogsAtEnd = await helper.blogsInDb()
-        assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
-
-        const titles = blogsAtEnd.map(res => res.title)
-        assert(titles.includes(newBlog.title))
+            const blogsAtEnd = await helper.blogsInDb()
+            const updatedBlog = blogsAtEnd.find(r => r.id === blogToUpdate.id)
+            assert.deepStrictEqual(updatedBlog, blogToUpdate)
+        })
     })
 
     after(async () => {
